e3c9df6729d2e9b38e16fae716f7805ba8099202
Apdoxdd <abdelrahmansameh69@gmail.com>
2026-09-04 18:33:39 +0300

    setup and cmake tests

---
+++ helloWorld/build/CMakeFiles/helloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloWorld.dir/link.d"
+  "CMakeFiles/helloWorld.dir/main.cpp.o"
+  "CMakeFiles/helloWorld.dir/main.cpp.o.d"
+  "helloWorld"
+  "helloWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
