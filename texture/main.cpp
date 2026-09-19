ec3b0e66a588bf01e680f2017092c7ab5f2dc1f3
Apdoxdd <abdelrahmansameh69@gmail.com>
2026-09-20 01:57:40 +0300

    texture setup

--- texture/main.cpp
+++ texture/main.cpp
@@ -6,7 +6,62 @@
 #include <iostream>
 
 
+void updateView(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow *window = glfwCreateWindow(800, 600, "Shader", NULL, NULL);
+
+	if (!window)
+	{
+		std::cout<<"failed to create a window"<<std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout<<"failed to initialize GLAD";
+		return -1;
+	}
+	
+	glfwSetFramebufferSizeCallback(window, updateView);
+
+	while(!glfwWindowShouldClose(window))
+	{
+		//input
+		processInput(window);
+
+
+		glClearColor(0.3f, 0.4f, 0.4f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+		
+}
+
+
+void updateView(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
