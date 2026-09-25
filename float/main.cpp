eda055d237c53a6280e83298981d8b7ea5cde696
Apdoxdd <abdelrahmansameh69@gmail.com>
2026-09-25 04:18:42 +0300

    main func init for float

---
+++ float/main.cpp
@@ -0,0 +1,63 @@
+#include "shader.hpp"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb_image.h>
+#include <cmath>
+#include <iostream>
+
+
+void updateView(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "SMILEY WALL", NULL, NULL);
+	if(!window)
+	{
+		std::cout<<"failed to create a window"<<std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout<<"failed to initialize GLAD";
+		return -1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, updateView);
+	stbi_set_flip_vertically_on_load(true);
+
+	while(!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+
+		glClearColor(0.0f, 0.2314f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+}
+
+void updateView(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
